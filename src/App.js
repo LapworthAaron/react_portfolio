ee1ad080d66fd407f109f4cb39c7092cd5131df8
Aaron Lapworth <aaronlapworth@hotmail.co.uk>
2023-03-03 18:48:40 +0000

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/pages/Home';
+import ProjectGallery from './components/pages/ProjectGallery';
+import Skills from './components/pages/Skills';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        {/* Wrap Route elements in a Routes component */}
+        <Routes>
+          {/* Define routes using the Route component to render different page components at different paths */}
+          <Route path="/" element={<Home />} />
+          <Route path="progGallery" element={<ProjectGallery />} />
+          <Route path="skills" element={<Skills />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Header() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <NavLink
+          to="about"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="progGallery"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Project Gallery
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="skills"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Skills
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
+
+export default Header;
